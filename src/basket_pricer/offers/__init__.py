05aee815889a146af824a13b7069e9019b57c821
vivan1999 <vivanverma38@gmail.com>
2026-02-16 00:22:22 +0000

    module issues fixed

--- src/basket_pricer/offers/__init__.py
+++ src/basket_pricer/offers/__init__.py
@@ -1,13 +1,11 @@
 from .base_offer import AbstractBaseOffer
 from .buy_x_get_cheapest_free_offer import BuyXGetCheapestFreeOffer
 from .buy_x_get_y_free import BuyXgetYfree
-from .offers_factory import OffersFactory
 from .percentage_discount import PercentageOffer
 
 __all__ = [
     "AbstractBaseOffer",
     "BuyXGetCheapestFreeOffer",
     "BuyXgetYfree",
-    "PercentageOffer",
-    "OffersFactory",
+    "PercentageOffer"
 ]
